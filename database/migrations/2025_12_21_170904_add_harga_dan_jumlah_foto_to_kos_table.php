e182625ddba114a51b76a90694bd018876083afd
Disya <email@dianaiffatul46.com>
2026-01-09 19:18:31 +0700

    change migration

--- database/migrations/2025_12_21_170904_add_harga_dan_jumlah_foto_to_kos_table.php
+++ database/migrations/2025_12_21_170904_add_harga_dan_jumlah_foto_to_kos_table.php
@@ -10,18 +10,20 @@ return new class extends Migration
      * Run the migrations.
      */
     public function up(): void
-{
-    Schema::table('kos', function (Blueprint $table) {
-        $table->integer('harga')->nullable()->after('deskripsi');
-        $table->integer('jumlah_foto')->default(0)->after('foto_kos');
-    });
-}
+    {
+        Schema::table('kos', function (Blueprint $table) {
+            $table->integer('harga')->nullable()->after('deskripsi');
+            $table->integer('jumlah_foto')->default(0)->after('foto_kos');
+        });
+    }
 
-public function down(): void4
-{
-    Schema::table('kos', function (Blueprint $table) {
-        $table->dropColumn(['harga', 'jumlah_foto']);
-    });
-}
-
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('kos', function (Blueprint $table) {
+            $table->dropColumn(['harga', 'jumlah_foto']);
+        });
+    }
 };
